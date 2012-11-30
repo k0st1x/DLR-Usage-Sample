4479f97d46e808ab5a1f1522201fdfbc566f0414
Konstantin <k0st1x@ya.ru>
2012-12-01 03:24:46 +0400

    hosting sample

---
+++ 06_hosting/1_simple/PyHosting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace PyHosting {
+    class Program {
+        static void Main(string[] args) {
+            var runtimeSetup = Python.CreateRuntimeSetup(new Dictionary<string, object>());
+            runtimeSetup.DebugMode = true;
+            
+            var runtime = new ScriptRuntime(runtimeSetup);
+
+            var scope = runtime.CreateScope(new Dictionary<string, object> {
+                { "name" , "Batman" }
+            });
+
+            var engine = runtime.GetEngine("py");
+            var scriptSource = engine.CreateScriptSourceFromFile("script.py");
+            var compiledCode = scriptSource.Compile();
+            
+            compiledCode.Execute(scope);
+        }
+    }
+}
---
+++ 06_hosting/1_simple/PyHosting/script.py
@@ -0,0 +1,2 @@
+﻿msg = "Who is " + name + "?"
+print(msg)
