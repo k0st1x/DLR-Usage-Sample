0f85ce1a449de014d6843d9b83464ca8f6f6e866
Konstantin <k0st1x@ya.ru>
2012-12-01 00:47:40 +0400

    first sample

---
+++ 01_cs_expression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ConsoleApplication4 {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        static void ExpressionTest() {
+            Expression<Func<int, string>> expr = i => i.ToString();
+            var func = expr.Compile();
+            func(42);
+        }
+
+        static void DynamicTest(dynamic a) {
+            var b = a + 10;
+        }
+    }
+}
